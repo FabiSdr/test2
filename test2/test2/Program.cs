b8e5138b3cc3ca5514c69513e1c4215bc123fa80
Fabian Sieder <02siefab@rgtfo-me.it>
2020-04-16 10:48:11 +0200

    Zentrierte Ausgabe

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 //*** Konvertierung in Zahlensysteme
+
 namespace test2
 {
     class Program
@@ -15,10 +17,16 @@ namespace test2
 
         static void Titel(string titel)
         {
+            int breite = Console.WindowWidth;
+            int textlen = titel.Length;
             Console.Clear();
             for (int i = 0; i < Console.WindowWidth; i++)
             {
                 Console.Write("*"); 
+            }
+            for (int i = 0; i < (breite-textlen)/2; i++)
+            {
+                Console.Write(" ");
             }
             Console.WriteLine(titel);
             for (int i = 0; i < Console.WindowWidth; i++)
