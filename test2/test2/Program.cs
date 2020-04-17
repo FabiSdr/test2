447f4a97de47d7bf177ad980e3a12824b6d1bbd1
Fabian Sieder <02siefab@rgtfo-me.it>
2020-04-17 09:53:53 +0200

    Revert "Einlesen Binärzahlen"
    This reverts commit 3956caafa97b9d2463f43bf9de6e293549f255a8.

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -11,17 +11,11 @@ namespace test2
     {
         static void Main(string[] args)
         {
-            Titel("TITEL DER ANWENDUNG", ConsoleColor.Blue);
+            Titel("Dies ist ein Test", ConsoleColor.Blue);
             Console.WriteLine("ttttt");
-            string Binzahl = BinaerzahlEinlesen();
-            
             Console.ReadKey();
         }
-        /// <summary>
-        /// Gibt einen Text formatiert als Titel aus
-        /// </summary>
-        /// <param name="titel">Text der ausgegeben werden soll</param>
-        /// <param name="textfarbe">ConsoleColor Textfarbe des Titels</param>
+
         static void Titel(string titel, ConsoleColor textfarbe)
         {
             int breite = Console.WindowWidth;
@@ -43,16 +37,6 @@ namespace test2
                 Console.Write("*");
             }
             Console.ForegroundColor = aktTextFarbe;
-        }
-        /// <summary>
-        /// Einlesen einer Binärzahl
-        /// </summary>
-        /// <returns>String mit nur Nullen und Einsen</returns>
-        static string BinaerzahlEinlesen()
-        {
-            Console.WriteLine("Bitte Binärzahl eingeben (0/1): ");
-            string input = Console.ReadLine();
-            return input;
         }
     }
 }
